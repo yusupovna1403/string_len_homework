4f3a2384466c0a68586fa3e1fe0bf9aeebd55d71
ikromyusupov <@ikromyusupovdev@gmail.com>
2022-12-17 20:58:14 +0500

    done

---
+++ string_len08.py
@@ -0,0 +1,11 @@
+def main(s):
+    """
+    Given variable type string s. Return the character in the middle.
+    If the length is even, return two characters in the middle.
+
+    Args:
+        s: str
+    Returns:
+        str: answer
+    """
+    return
