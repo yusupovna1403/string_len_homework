dea2d412df99715cfee0b1be227b1e8bc48063ab
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-02 17:24:09 +0500

    string_len_function07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,23 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    n1 = len(s1)
+    n2 = len(s2)
+    n3 = len(s3)
+    if n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 1:
+        return f'[{s1},{s2},{s3}]'
+    elif n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 0:
+        return f'[{s1},{s2}]'
+    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 1:
+        return f'[{s1},{s3}]'
+    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 1:
+        return f'[{s2},{s3}]'
+    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 0:
+        return f'[{s1}]'
+    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 0:
+        return f'[{s2}]'
+    elif n1 % 2 == 0 and n2 % 2 == 0 and n3 % 2 == 1:
+        return f'[{s3}]'
+    else:
+        return f'[]'
+print(main("coder","code","python"))
