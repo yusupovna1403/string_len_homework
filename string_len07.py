b8f7dca484147dbb4c611b03eb46d871ed6fd1ce
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-03 17:27:28 +0500

    string

--- string_len07.py
+++ string_len07.py
@@ -12,27 +12,20 @@ def main(s1,s2,s3):
     n2 = len(s2)
     n3 = len(s3)
     list = []
-    #if n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 1:
-        #return f'[{s1},{s2},{s3}]'
-    #elif n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 0:
-        #return f'[{s1},{s2}]'
-    #elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 1:
-        #return f'[{s1},{s3}]'
-    #elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 1:
-        #return f'[{s2},{s3}]'
-    #elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 0:
-        #return f'[{s1}]'
-    #elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 0:
-        #return f'[{s2}]'
-    #elif n1 % 2 == 0 and n2 % 2 == 0 and n3 % 2 == 1:
-        #return f'[{s3}]'
-    #else:
-        #return f'[]'
-    if n1 % 2 == 1:
-        list.append(s1)
-    if n2 % 2 == 1:
-        list.append(s2)
-    if n3 % 2 == 1:
-        list.append(s3)
-    return list
+    if n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 1:
+        return f'[{s1},{s2},{s3}]'
+    elif n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 0:
+        return f'[{s1},{s2}]'
+    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 1:
+        return f'[{s1},{s3}]'
+    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 1:
+        return f'[{s2},{s3}]'
+    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 0:
+        return f'[{s1}]'
+    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 0:
+        return f'[{s2}]'
+    elif n1 % 2 == 0 and n2 % 2 == 0 and n3 % 2 == 1:
+        return f'[{s3}]'
+    else:
+        return f'[]'
 print(main("coder","code","python"))
