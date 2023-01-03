bfcb08f178b0a8410023d579592ec77893f65f23
yusupovna1403 <yusupovna2003girl@gmail.com>
2023-01-03 17:24:28 +0500

    string_len_function07

--- string_len07.py
+++ string_len07.py
@@ -1,6 +1,6 @@
 def main(s1,s2,s3):
     """
-    Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
+    Given three strings, s1, s2 and s3. find their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
     Args:
         s1: string
         s2: string
@@ -11,20 +11,28 @@ def main(s1,s2,s3):
     n1 = len(s1)
     n2 = len(s2)
     n3 = len(s3)
-    if n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 1:
-        return f'[{s1},{s2},{s3}]'
-    elif n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 0:
-        return f'[{s1},{s2}]'
-    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 1:
-        return f'[{s1},{s3}]'
-    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 1:
-        return f'[{s2},{s3}]'
-    elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 0:
-        return f'[{s1}]'
-    elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 0:
-        return f'[{s2}]'
-    elif n1 % 2 == 0 and n2 % 2 == 0 and n3 % 2 == 1:
-        return f'[{s3}]'
-    else:
-        return f'[]'
+    list = []
+    #if n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 1:
+        #return f'[{s1},{s2},{s3}]'
+    #elif n1 % 2 == 1 and n2 % 2 == 1 and n3 % 2 == 0:
+        #return f'[{s1},{s2}]'
+    #elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 1:
+        #return f'[{s1},{s3}]'
+    #elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 1:
+        #return f'[{s2},{s3}]'
+    #elif n1 % 2 == 1 and n2 % 2 == 0 and n3 % 2 == 0:
+        #return f'[{s1}]'
+    #elif n1 % 2 == 0 and n2 % 2 == 1 and n3 % 2 == 0:
+        #return f'[{s2}]'
+    #elif n1 % 2 == 0 and n2 % 2 == 0 and n3 % 2 == 1:
+        #return f'[{s3}]'
+    #else:
+        #return f'[]'
+    if n1 % 2 == 1:
+        list.append(s1)
+    if n2 % 2 == 1:
+        list.append(s2)
+    if n3 % 2 == 1:
+        list.append(s3)
+    return list
 print(main("coder","code","python"))
